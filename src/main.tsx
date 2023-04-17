88658c727202f02bb85efa9e00525e96b93b9ee0
Anurag Lodhi <32013268+AnuragLodhi@users.noreply.github.com>
2023-04-17 10:33:56 +0000

    Created store

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import Editor from "./Editor";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Editor />
+    <Provider store={store}>
+      <Editor />
+    </Provider>
   </React.StrictMode>
 );
