b49188c7031cf361e999faa99e0972a6ab895cef
Anurag Lodhi <anuraglodhi63@gmail.com>
2023-05-01 20:20:45 +0530

    Added upload functionality

--- src/Share.tsx
+++ src/Share.tsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import cube from "/src/assets/cube.jpg";
 import test from "/src/assets/test.jpg";
+import { useDispatch } from "react-redux";
+import { setImage } from "./features/image/imageSlice";
+import { useSelector } from "react-redux";
 
 const Share = () => {
+  const dispatch = useDispatch();
+  const image = useSelector((state: any) => state.image);
+
+  const handleUpload = (e: FormEvent) => {
+    const input = e.target as HTMLInputElement;
+    const file = input.files ? input.files[0] : null;
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = (event) => {
+      const url = `data:${file.type};base64,${btoa(
+        event.target?.result as string
+      )}`;
+      dispatch(setImage(url));
+    };
+  };
+
   return (
     <>
       <header className="flex h-16 items-center bg-slate-200 px-4 text-3xl font-bold">
         Simple Image Editor
       </header>
       <main>
-        <section className="h-[200px] bg-slate-100"></section>
+        <section className="h-[200px] bg-slate-100">
+          <input type="file" onChange={handleUpload} />
+        </section>
         <section className="flex flex-col items-center p-4">
           <div className="max-w-screen-lg">
             <h2 className="w-full text-xl">See what others have done</h2>
