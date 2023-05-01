1a8a6398a63022738bba085b490b048ebe3bc620
Anurag Lodhi <anuraglodhi63@gmail.com>
2023-05-01 20:20:30 +0530

    Added redux slice for uploaded image

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from "../features/filter/filterSlice";
+import imageReducer from "../features/image/imageSlice";
 
 export const store = configureStore({
     reducer: {
         filter: filterReducer,
+        image: imageReducer,
     },
 });
 
